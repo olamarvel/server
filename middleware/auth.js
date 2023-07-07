eeb7b5b1b72a9e5b4049db922a4dcae2cf2728bf
olamarvel <olatundemarvelousanthony@gmailcom>
2023-07-07 11:09:12 +0100

    latest production

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,4 +16,20 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-module.exports = { verifyToken };
+const verifyGetRouteToken = (req, res, next) => {
+  const getToken = req.params.token
+  if (!getToken) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(getToken, process.env.JWT_SECRET);
+    req.user = decoded; // Attach the decoded user information to the request object
+    next();
+  } catch (err) {
+    console.log(err)
+    return res.status(403).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = { verifyToken,verifyGetRouteToken };
